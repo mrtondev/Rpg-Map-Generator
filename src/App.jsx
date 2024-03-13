bc8db1b91a36755f02e8a96e4c47f8503dcc4a82
Ayrton Rafael <mrton.developer@gmail.com>
2024-03-12 23:38:53 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+        <div className='landing'>
+          <h1> Rpg Map generator </h1>
+
+          <div className='content'>
+            
+          </div>
+        </div>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
