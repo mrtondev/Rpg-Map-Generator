89693c82062a0a7c67dd3185f67c2365c92208ed
Ayrton Rafael <mrton.developer@gmail.com>
2024-03-17 20:11:46 -0300

    Places and Directions

--- src/generateMap.jsx
+++ src/generateMap.jsx
@@ -4,15 +4,22 @@ function criador(){
 
      const direcoes = ['Norte','Sul', 'Oeste','Leste']
 
-     const names = ['Floresta', 'Cidade','Neve', 'Dungeon','Deserto','Praia']
+     const places = ['Floresta', 'Cidade','Neve', 'Dungeon','Deserto','Praia']
     alert('chamada ok');
 
-
-    
     let direcao = Math.floor(Math.random() * direcoes.length)
-    let nomes = Math.floor(Math.random() * names.length)      
 
-    console.log(`${direcoes[direcao]}  ${names[nomes]}`)
+     while (direcao <= 3){
+
+          let nomes = Math.floor(Math.random() * places.length)
+
+          console.log(`${direcoes[direcao]}  ${places[nomes]}`)   
+          
+          direcao++
+     }
+    
+    
+
 }
 
 
